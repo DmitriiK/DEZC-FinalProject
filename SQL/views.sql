f6cd2cb91ed60d6335018a50572d38e34b09c98e
Dmitrii Kalmanovich <dklmn@mail.ru>
2023-03-26 21:12:41 +0000

    new atributes

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,14 +1,28 @@
 create or replace view v_emlak as 
-select  eml.id, dc.city_name, c.country_name, d.district_name, 
-	eml.room, eml.livingroom , dft.floor_type_name ,
-	eml.is_furnished, eml.price , eml.sqm_netsqm, eml.maplocation_lat, eml.maplocation_lon,  eml.price/ eml.sqm_netsqm as sqm_price
+with most_recent_load as (select  fl2.load_id  from f_loads fl2 order by fl2.load_id desc limit 1) 
+select  eml.id, 
+	eml.createdate, eml.updateddate, fl.dt_start as last_load_date,
+	case when eml.load_id =mrl.load_id then 1 else 0 end as is_most_recent_load,
+  	eml.city_id,  dc.city_name
+   ,eml.country_id, c.country_name
+   ,eml.district_id,  d.district_name, 
+	eml.room,  eml.livingroom , dft.floor_type_name ,
+	eml.is_furnished, eml.price , eml.sqm_netsqm, eml.maplocation_lat, eml.maplocation_lon,  
+	calc.sqm_price
+	,case when eml.is_furnished then calc.sqm_price else 0 end sqm_price_furnished
+	,case when eml.is_furnished then 1 else 0 end cnt_furnished
+	,case when not eml.is_furnished then calc.sqm_price else 0 end sqm_price_not_furnished
+	,case when not eml.is_furnished then 1 else 0 end cnt_not_furnished
 	FROM public.f_emlak eml
 	join d_countries c on c.country_id =eml.country_id
 	join d_districts d on d.district_id =eml.district_id
 	join d_cities dc on dc.city_id =eml.city_id 
 	join d_floor_type dft on dft.floor_type_id =eml.floor_type_id 
+	join f_loads fl  on fl.load_id =eml.load_id
+	cross join most_recent_load mrl,
+	lateral (select eml.price/eml.sqm_netsqm as sqm_price) calc
 	where  eml.price/ eml.sqm_netsqm between 40 and 500
-	--limit 100 
+	--limit 100 	
 
 create or replace view v_emlak_aggr_by_geo as 	
 select   city_id,  x.city_name, x.country_name, x.district_name
